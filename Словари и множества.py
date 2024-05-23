2102a50eb715c518ca0b591ec641e5dc5316eb18
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-05-23 22:40:03 +0300

    Update Словари и множества.py

--- Словари и множества.py
+++ Словари и множества.py
@@ -5,13 +5,13 @@ print(("Maria's B-day:"), (my_dict.get('Maria')))
 my_dict['Billie'] = 1990
 my_dict.update({'Craig': 2010})
 del my_dict['Peter']
-print(('B-Day dictionary:'), (my_dict))
+print(my_dict.get('Peter'))
+print(('Modified B-Day dictionary:'), (my_dict))
 
 my_set = {1, 2, 3, 3, 2, 1, 'Cola', True, False, False}
 print(('Set:'), (my_set))
 my_set.add(4)
 my_other_set = {12, 12, 'Cola'}
 my_set.update(my_other_set)
-print(('Set:'), (my_set))
 my_set.discard('Cola')
-print(('Set:'), (my_set))
+print(('Modified set:'), (my_set))
